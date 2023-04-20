0b07f9473734c1778c9591620d3f3746dc5803ed
KangnaV1 <kangnaverma1@gmail.com>
2023-04-20 19:40:35 +0530

    ngs2

--- ngs-class02.R
+++ ngs-class02.R
@@ -1,3 +1,39 @@
 # BIG HEADER
 
 ### SMALL HEADER
+
+``` {R}
+# vectors
+
+# character vector, numeric vector, logical vector & factor
+
+# single element
+char_vec = 'hello'
+
+#multiple elements
+
+char_vec_many = c('a', 'c', 'h')
+
+#numeric vector
+num_vector <- c(1, 4.5, 6, 7)
+
+#character vector; if any one is also a character, all will be considered character
+
+xg = c(2, "a", 10)
+
+#logical vector
+logical_vec <- char_vec_many == 'c'
+logical_vec
+logical_vec [1]
+logical_vec[2]
+char_vec_many[2]
+x <- char_vec_many[2]
+x
+char_vec_many[logical_vec]
+char_vec_many[!logical_vec]
+
+ 
+
+```
+
+
